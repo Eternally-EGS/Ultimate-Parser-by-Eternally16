f7e6bd6d79e35cf0e79abbcc34c6f64ec4dd9759
Eternally16 <eternally1615@gmail.com>
2026-06-14 21:39:04 +0300

    Create simple parser Yo

--- Program.cs
+++ Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using AngleSharp;
+using AngleSharp.Dom;
+
+
+// Default config settings
+var config = Configuration.Default.WithDefaultLoader();
+
+// Creating browser
+var context = BrowsingContext.New(config);
+
+try {
+
+// Create base page
+var document = await context.OpenAsync("https://lenta.ru");
+
+// Parsing point
+var titles = document.QuerySelectorAll("h3");
+
+try {
+
+// Output
+Console.WriteLine($"Я нихуя не понимаю количество заголовков: {titles.Length}");
+
+
+foreach(var tit in titles){
+    Console.WriteLine(tit.TextContent.Trim());
+}
+
+} catch(Exception ex) {
+Console.WriteLine($"Ошибка: {ex}");
+
+}
+
+} catch (Exception ex) {
+    Console.WriteLine($"Ошибка подключения: {ex}");
+}
